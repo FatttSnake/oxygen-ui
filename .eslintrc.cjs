ecbe15621576ad6941555b5229e777f77a00c916
FatttSnake <fatttsnake@gmail.com>
2023-09-06 18:20:56 +0800

    Fix conflicts between eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,11 +24,10 @@ module.exports = {
     rules: {
         'no-cond-assign': 'error',
         'eqeqeq': 'error',
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
         'prettier/prettier': [
             'error',
             {
-                endOfLine: 'auto'
+                endOfLine: 'auto',
             }
         ],
         'react-refresh/only-export-components': [
