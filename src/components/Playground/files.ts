34034ec59b376b7548c5387c08e0dfde2ea3ea9b
FatttSnake <fatttsnake@gmail.com>
2024-01-24 17:03:56 +0800

    Optimize Playground

--- src/components/Playground/files.ts
+++ src/components/Playground/files.ts
@@ -1,10 +1,10 @@
 import { strFromU8, strToU8, unzlibSync, zlibSync } from 'fflate'
 import { languages } from 'monaco-editor'
 import DiagnosticsOptions = languages.json.DiagnosticsOptions
+import { formatByteSize } from '@/util/common'
 import { IFile, IFiles, ILanguage } from '@/components/Playground/shared'
 import tsconfigSchema from '@/components/Playground/tsconfig-schema.json'
 import importMapSchema from '@/components/Playground/import-map-schema.json'
-import { formatByteSize } from '@/util/common.tsx'
 
 export const TS_CONFIG_FILE_NAME = 'tsconfig.json'
 export const MAIN_FILE_NAME = 'App.tsx'
