20cb91a71948902364d24bd7efdedbc1e2711e0c
FatttSnake <fatttsnake@gmail.com>
2025-06-07 01:59:25 +0800

    perf(statistics): optimize online user statistics chart loading performance

--- src/pages/System/Statistics/ActiveInfo.tsx
+++ src/pages/System/Statistics/ActiveInfo.tsx
@@ -167,8 +167,8 @@ const ActiveInfo = () => {
                     >
                         <AntdSelect.Option key={'WEEK'}>最近7天</AntdSelect.Option>
                         <AntdSelect.Option key={'MONTH'}>最近30天</AntdSelect.Option>
-                        <AntdSelect.Option key={'QUARTER'}>最近3个月</AntdSelect.Option>
-                        <AntdSelect.Option key={'YEAR'}>最近12个月</AntdSelect.Option>
+                        <AntdSelect.Option key={'QUARTER'}>最近3月</AntdSelect.Option>
+                        <AntdSelect.Option key={'YEAR'}>最近12月</AntdSelect.Option>
                         <AntdSelect.Option key={'TWO_YEARS'}>最近2年</AntdSelect.Option>
                         <AntdSelect.Option key={'THREE_YEARS'}>最近3年</AntdSelect.Option>
                         <AntdSelect.Option key={'FIVE_YEARS'}>最近5年</AntdSelect.Option>
